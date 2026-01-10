b3509a14974a7e1725af1c63ed0fad0e15bbf094
Patra2020 <96439859+Patra2020@users.noreply.github.com>
2026-01-10 23:53:40 +0000

    Deploying to gh-pages from @ Patra2020/Patra2020.github.io@5e338bb39be67cd3212e332dc5ba0fc7eacbccd2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Patra2020/Patra2020.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
